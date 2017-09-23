f7c7c219d419e05418f26a444287d8e2e5cf55dc
Nitesh Khanna <nitesh.khanna@newgen.co.in>
2017-09-23 23:11:46 +0530

    Changed url from absolute to relative

--- app/js/services/AuthenticationService.js
+++ app/js/services/AuthenticationService.js
@@ -7,7 +7,7 @@ angular.module('AuthenticationService', []).factory('AuthenticationService', ['$
     return service;
 
     function Login(username, password, callback) {
-        $http({method:'POST',url:'https://patientproviders.herokuapp.com/authenticate', data: 'username='+ username+ '&password='+ password , headers:{'Content-Type': 'application/x-www-form-urlencoded'}})
+        $http({method:'POST',url:'/authenticate', data: 'username='+ username+ '&password='+ password , headers:{'Content-Type': 'application/x-www-form-urlencoded'}})
             .success(function (response) {
                 // login successful if there's a token in the response
                 if (response.token) {
@@ -36,7 +36,7 @@ angular.module('AuthenticationService', []).factory('AuthenticationService', ['$
     }
 
     function Register(username, password, callback) {
-        $http({method:'POST',url:'https://patientproviders.herokuapp.com/register', data: 'username='+ username+ '&password='+ password , headers:{'Content-Type': 'application/x-www-form-urlencoded'}})
+        $http({method:'POST',url:'/register', data: 'username='+ username+ '&password='+ password , headers:{'Content-Type': 'application/x-www-form-urlencoded'}})
             .success(function (response) {
                 // login successful if there's a token in the response
                 if (response.success) {
