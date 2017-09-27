1a7791c587d475e554798e689577e88e740edc59
Nitesh Khanna <nitesh.khanna@newgen.co.in>
2017-09-27 16:06:22 +0530

    Routes logic fix

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-var app=angular.module('app', ['infinite-scroll','ui.grid','ui.grid.resizeColumns', 'ui.grid.infiniteScroll','ui.select','ngRoute', 'ngStorage','appRoutes', 'LoginCtrl', 'AuthenticationService', 'PatientCtrl', 'PatientService']);
+var app = angular.module('app', ['infinite-scroll', 'ui.grid', 'ui.grid.resizeColumns', 'ui.grid.infiniteScroll', 'ui.select', 'ngRoute', 'ngStorage', 'appRoutes', 'LoginCtrl', 'AuthenticationService', 'PatientCtrl', 'PatientService']);
 app.run(run);
 
 function run($rootScope, $http, $location, $localStorage) {
@@ -12,7 +12,13 @@ function run($rootScope, $http, $location, $localStorage) {
         var publicPages = ['/'];
         var restrictedPage = publicPages.indexOf($location.path()) === -1;
         if (restrictedPage && !$localStorage.currentUser) {
+
             $location.path('/');
+        }
+        else if (restrictedPage && $location.path() !== '/patientproviders') {
+
+            $location.path('/');
+
         }
     });
 }
